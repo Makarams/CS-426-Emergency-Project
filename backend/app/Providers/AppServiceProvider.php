8c4d798cbf3529b88d3a2af940b312b866e3d3a4
Makara <sammakarams@gmail.com>
2024-03-02 11:01:28 +0700

    docker final

---
+++ backend/app/Providers/AppServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            base_path('config/api.php'),
+            'api'
+        );
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+}
---
+++ emergency.sql
@@ -0,0 +1,181 @@
+use emergency;
+
+INSERT INTO location (location_name, address, position_latitude, position_longitude)
+VALUES
+    ('Toul Kork', 'HVJX+VW7, National Road 1, Phnom Penh', 11.588824983679967, 104.90147939275673),
+    ('Toul Kork', 'HV8V+2WH, No. 14, St. 259, St 259, Phnom Penh 12156', 11.56647486326789, 104.89470094401507),
+    ('Toul Kork', 'HW52+629, St 230, Phnom Penh', 11.558975190901839, 104.90002512850295),
+
+    ('Toul Kork', '99 Russian Federation Blvd (110), Phnom Penh', 11.569191934070185, 104.88992392962915),
+    ('Toul Kork', '# 207, Street 182, Sangkat Phsar Depot I, 12153', 11.565239822608126, 104.90142524162427),
+    ('Toul Kork', '155AB Jawaharlal Nehru Blvd (215), Phnom Penh', 11.564314852294297, 104.90734755899487),
+
+    ('Chbar Ampov', 'Street Polaris, Phnom Penh', 11.526008824620746, 104.95767644593161),
+    ('Sen Sok', 'JVP7+75V, Unnamed Road, Phnom Penh', 11.63960582432184, 104.86259221950526),
+    ('Koh Pich', 'HW2V+52W, Koh Pich St, Phnom Penh', 11.554108454080849, 104.94394629060645),
+
+    ('Sen Sok', 'HVX7+79W, Phnom Penh', 11.601174654028044, 104.86310720403769),
+    ('Sen Sok', 'HVHH+765, Street 1972, Phnom Penh', 11.581790256365133, 104.8791675113063),
+    ('Sen Sok', 'No 03-4, Str. 1019 (Hanoi), Russei Keo, Street 1972', 11.580949423957255, 104.86972613550218),
+
+    ('Sen Sok', 'Phnom Penh, Unnamed Road', 11.62596583538924, 104.84794735868164),
+    ('Sen Sok', '135-137 street 1928, Phnom Penh 12411', 11.5889726746689, 104.84228253322495),
+    ('Sen Sok', 'HRHR+4F3, Street 105K, Phnom Penh', 11.582414284659487, 104.84159588772096),
+
+    ('Chbar Ampov', 'FXM3+5R2, Phnom Penh', 11.486680056407748, 104.95553612668323),
+
+    ('Chbar Ampov', '1 NR1, Phnom Penh', 11.536738608973538, 104.96122685395684),
+    ('Chbar Ampov', 'GWJQ+GR6, Street 638, Phnom Penh', 11.533768093962589, 104.93970036486483),
+    ('Chbar Ampov', 'NR1, Phnom Penh', 11.540327623100346, 104.96167302108762),
+
+    ('Koh Pich', 'Yale Road, Phnom Penh 12000', 11.543977629676123, 104.93813908803153),
+    ('Koh Pich', 'GWVQ+84 Phnom Penh', 11.543616528409192, 104.93778392668982),
+
+    ('Daun Penh', 'HWG9+WVF, Phnom Penh', 11.578596004919442, 104.91972326677576),
+
+    ('Daun Penh', '8F, No, Central Hospital, 82 Street 154, Phnom Penh 120203', 11.56861972271243, 104.92338776553638),
+    ('Daun Penh', '34 Preah Monivong Blvd (93), Phnom Penh', 11.582884622714092, 104.91504549970198),
+    ('Daun Penh', 'HWCF+M98, Jok Dimitrov Blvd, Phnom Penh', 11.572458164967934, 104.92414355264992),
+
+    ('Chroy Changvar', 'JWGC+6W3, Unnamed Road, Phnom Penh', 11.63038997250332, 104.92263078657273),
+    ('Chroy Changvar', 'JWH6+XJ4, Phnom Penh', 11.632575743635737, 104.91267442676488),
+    ('Chroy Changvar', 'MXP4+W98, Phnom Penh', 11.690616465078037, 104.95648158633797),
+
+    ('Chroy Changvar', 'Corner of Aphivath Street, House AP1-1130, 1st floor, Brahma roundabout, distance 950, m 121001', 11.600286970634746, 104.92501284031967),
+    ('Chroy Changvar', '177E, Kola Loum Street (the Bay Road), Group 2, Phum 2, Sangkat, Phnom Penh', 11.581749503021669, 104.93105292310213),
+    ('Chroy Changvar', 'Keo Chenda St, Phnom Penh 12110', 11.589289827465667, 104.93377972855082),
+    
+    ('Porsenchey', 'GRMF+9RH, Phnom Penh', 11.537131549681117, 104.82542626204317),
+    ('Porsenchey', 'HV74+2G8, Phnom Penh', 11.566227430361476, 104.85684029385065),
+    ('Porsenchey', 'HR8R+G97, Phnom Penh', 11.568372613026641, 104.84124317066434),
+
+    ('Porsenchey', '102 Street 68k, Phnom Penh', 11.563411428369491, 104.83961238759237),
+    ('Porsenchey', 'HR2Q+5GM, Phnom Penh', 11.551994387252611, 104.83839677335514),
+    ('Porsenchey', 'GQFQ+25C, Phnom Penh', 11.524622263654246, 104.7889998987432),
+
+    ('Kamboul', 'GQM6+R7G, Phnom Penh', 11.537519698891785, 104.76095549481461),
+
+    ('Kamboul', 'GQFM+866, Phnom Penh', 11.527728204246984, 104.78333594644047),
+    ('Kamboul', '#street42,preyboung,kantouk, Phnom Penh', 11.524484440779656, 104.78780697991758),
+
+    ('Chamkar Mon', 'GWPG+JR7, CID, Cambodia, Oknha Nhek Tioulong St. (466), Phnom Penh', 11.538120106656534, 104.92649853202654),
+
+    ('Chamkar Mon', 'Phnom, Yuthapol Khmemarak Phoumin Blvd, Penh 12306', 11.54644550764736, 104.90375339969599),
+    ('Chamkar Mon', '#28, Oknha Chrun You Hak St. (294), Phnom Penh', 11.554691825941878, 104.92555439445005),
+
+    ('Boeng Keng Kang', 'HW2G+9XF, St 322, Phnom Penh', 11.551881484963168, 104.92730713711511),
+    ('Boeng Keng Kang', '19 St 374, Phnom Penh', 11.548055207359393, 104.9090208576469),
+    ('Boeng Keng Kang', 'No. 238, European Union (St. 143), Phnom Penh 12304', 11.548527093430572, 104.91632566135091),
+
+    ('Boeng Keng Kang', 'HW2F+FHC, Preah Trasak Paem St. (63), Phnom Penh', 11.552251720982195, 104.92410155482871),
+
+    ('Dangkao', 'Dangkao Police Station, Phnom Penh', 11.511766589972266, 104.89296868297062),
+
+    ('Dangkao', 'GV3R+QMJ, Chamkar Doung Street (217), Phnom Penh 12401', 11.505391780459142, 104.89213207784118),
+
+    ('Mean Chey', 'Phnom Penh Gendarmerie, Veng Sreng Blvd, Phnom Penh', 11.536539484995048, 104.8848370664225),
+
+    ('Mean Chey', '66, Street 31cc, 3, Phnom Penh 120605', 11.538053583527619, 104.86925880198258),
+
+    ('Russey Keo', 'Russey Keo Police Office, Phnom Penh', 11.62939870864598, 104.90416161851604),
+
+    ('Preaek Pnov', 'National Highway 5, Prek Pnov Bridge, 12401', 11.66269167206234, 104.86206711364562),
+
+    ('Prampi Makara', 'HW76+C6R, Oknha Tep Phan, Phnom Penh', 11.564137464079126, 104.91042271158982),
+
+    ('Prampi Makara', 'No. 137-139, Czech Republic Blvd (169), corner of Street 134, Phnom Penh 12253', 11.567975776869387, 104.91256106019678);
+
+INSERT INTO general_department (department_name, department_specialty, department_type, legend_symbol, phone_number, location_id)
+VALUES
+    ('Military Police Headquarter', 'Military', 'Police', 'images/map-marker_1.png', ('012345678'), 1),
+    ('TEUK LAAK 1 Administrative Police Post', 'Administrative', 'Police', 'images/map-marker_1.png', ('016893811'), 2),
+    ('Anti-Crime Police Office', 'Anti-Crime', 'Police', 'images/map-marker_1.png', ('069449999'), 3),
+
+    ('Pacific Phnom Penh Hospital', 'General', 'Medical', 'images/map-marker_2.png', ('011959625'), 4),
+    ('Chinese Hengda Womens and Childrens Hospital', 'Womens and Childrens', 'Medical', 'images/map-marker_2.png', ('010334422'), 5),
+    ('Long Hua Zhong Yi Yuan Polyclinic', 'Polyclinic', 'Medical', 'images/map-marker_2.png', ('017997349'), 6),
+
+    ('Grand Star Platinum Fire Station, Phnom Penh, Cambodia', 'Fire station', 'Fire', 'images/map-marker_3.png', ('016596789'), 7),
+    ('Phnom Penh Municipal Fire Police Brigade', 'Fire station', 'Fire', 'images/map-marker_3.png', ('012345678'), 8),
+    ('Koh Pich Fire Station', 'Fire station', 'Fire', 'images/map-marker_3.png', ('012345678'), 9),
+    
+    ('Sensok District Inspectors hand quarter', 'Traffic', 'Police', 'images/map-marker_1.png', ('012345678'), 10),
+    ('Government Office and Poste Dela Police ADM Phnom Penh Thmey', 'Government', 'Police', 'images/map-marker_1.png', ('012670116'), 11),
+    ('Poste de la Police ADM Khan Sen Sok', 'General', 'Police', 'images/map-marker_1.png', ('012345678'), 12),
+
+    ('Sensok Cambodia China Friendship Referral Hospital', 'Referral', 'Medical', 'images/map-marker_2.png', ('012345678'), 13),
+    ('Bakong Sensok Polyclinic', 'Polyclinic', 'Medical', 'images/map-marker_2.png', ('098900441'), 14),
+    ('SENSOK CLINIC', 'Clinic', 'Medical', 'images/map-marker_2.png', ('012345678'), 15),
+
+    ('Administrative of Police Post Prek Thmei', 'Administrative', 'Police', 'images/map-marker_1.png', '012345678', 16),
+
+    ('Cho Ray Phnom Penh Hospital', 'General', 'Medical', 'images/map-marker_2.png', '0236841001', 17),
+    ('CHBAR AMPOV POLYCLINIC', 'Polyclinic', 'Medical', 'images/map-marker_2.png', '023720200', 18),
+    ('SomrithMonika Clinic & Maternity', 'Clinic', 'Medical', 'images/map-marker_2.png', '077899894', 19),
+
+    ('Orchid Koh Pich Hospital', 'General', 'Medical', 'images/map-marker_2.png', '012322025', 20),
+    ('Élysée KohPich Polyclinic', 'Polyclinic', 'Medical', 'images/map-marker_2.png', '077696949', 21),
+
+    ('Wat Phnom Police Station', 'General', 'Police', 'images/map-marker_1.png', '012345678', 22),
+
+    ('Japan Eye Hospital', 'Ophthalmology', 'Medical', 'images/map-marker_2.png', '087465283', 23),
+    ('Calmette Hospital', 'General', 'Medical', 'images/map-marker_2.png', '023426948', 24),
+    ('Preah Ang Duong Hospital', 'General', 'Medical', 'images/map-marker_2.png', '023218875', 25),
+
+    ('Chroy Changvar Police Inspectorate', 'Federal', 'Police', 'images/map-marker_1.png', ('012345678'), 26),
+    ('Prek Ta Sek Administrative Police Post', 'Administrative ', 'Police', 'images/map-marker_1.png', ('012345678'), 27),
+    ('Kaoh Dach Administrative of Police Post', 'Administrative', 'Police', 'images/map-marker_1.png', ('012345678'), 28),
+
+    ('La Paix Medical Center', 'Medical Center', 'Medical', 'images/map-marker_2.png', '099852030', 29),
+    ('Sunrise Japan Hospital Phnom Penh', 'General', 'Medical', 'images/map-marker_2.png', '023260152', 30),
+    ('Cambodian Family Clinic F.C.C', 'Clinic', 'Medical', 'images/map-marker_2.png', '010818150', 31),
+
+    ('Chaom Chau Administrative of Police Post', 'Administrative', 'Police', 'images/map-marker_1.png', ('010276841'), 32),
+    ('Police Post', 'Civil ', 'Police', 'images/map-marker_1.png', ('012345678'), 33),
+    ('Kakap II Police Station', 'State', 'Police', 'images/map-marker_1.png', ('012345678'), 34),
+
+    ('Hebron Medical Center', 'Medical Center', 'Medical', 'images/map-marker_2.png', '0236336119', 35),
+    ('New Phnom Penh Pediatric Clinic', 'Clinic', 'Medical', 'images/map-marker_2.png', '012345678', 36),
+    ('Rim Sokuntheary Hospital', 'General', 'Medical', 'images/map-marker_2.png', '012345678', 37),
+
+    ('Kambol District Police Inspectorate', 'Inspectorate', 'Police', 'images/map-marker_1.png', ('0888838490'), 38),
+
+    ('Kambol Health Center', 'Health Center', 'Medical', 'images/map-marker_2.png', '012345678', 39),
+    ('Rim Sokunthea Cabinet', 'Cabinet', 'Medical', 'images/map-marker_2.png', '012345678', 40),
+
+    ('Crime Investigation Department', 'Investigation', 'Police', 'images/map-marker_1.png', ('0888003167'), 41),
+
+    ('KHMER -SOVIET FRIENDSHIP HOSPITAL', 'General', 'Medical', 'images/map-marker_2.png', '023217524', 42),
+    ('Khema International Polyclinic', 'Polyclinic', 'Medical', 'images/map-marker_2.png', '098911811', 43),
+
+    ('Sangkat Police BKK1', 'General', 'Police', 'images/map-marker_1.png', ('012345678'), 44),
+    ('Police Inspection of Boeung Keng Kang District', 'Inspection', 'Police', 'images/map-marker_1.png', ('012475757'), 45),
+    ('Poste Dela Police A.D.M Boeung Keng Kang III', 'General', 'Police', 'images/map-marker_1.png', ('012931631'), 46),
+
+    ('Mekong Clinic', 'Clinic', 'Medical', 'images/map-marker_2.png', '012345678', 47),
+
+    ('Police Inspection of Dangkor District', 'Inspection', 'Police', 'images/map-marker_1.png', ('0888787198'), 48),
+
+    ('Dangkao Referral Hospital', 'Referral', 'Medical', 'images/map-marker_2.png', '012345678', 49),
+
+    ('Royal Gendarmerie of Phnom Penh', 'Gendarmerie', 'Police', 'images/map-marker_1.png', ('0313556322'), 50),
+
+    ('Orienda International Hospital', 'General', 'Medical', 'images/map-marker_2.png', '023232789', 51),
+
+    ('Gendarmerie Rusey Keo', 'Gendarmerie', 'Police', 'images/map-marker_1.png', ('012345678'), 52),
+
+    ('Prek Pneou Referral Hospital', 'Referral', 'Medical', 'images/map-marker_2.png', '012345678', 53),
+
+    ('Inspection of Prampey Makara', 'Gendarmerie', 'Police', 'images/map-marker_1.png', ('012345678'), 54),
+
+    ('Phnom Penh Municipal Referral Hospital', 'Referral', 'Medical', 'images/map-marker_2.png', '023881612', 55);
+
+INSERT INTO personal_record (first_name, last_name, occupation, position, phone_number, department_id, account_id)
+VALUES
+    ('Long', 'Seng', 'Policeman', 'Domestic Violence Officer', '0972410355', 1, 1),
+    ('Chumvisotdaroth', 'Som', 'Investigator', 'Fraud Investigator', '095226392', 1, 1),
+    ('Chanpichmony', 'En Soth', 'Detective', 'Homicide Detective', '092969565', 1, 1),
+    
+    ('Headangelly', 'Huy', 'Doctor', 'Surgeon', '012345678', 4, 1),
+    ('Vongsorya', 'Uch', 'Doctor', 'Anesthesiologist', '016222498', 4, 1),
+    ('Sok Eng', 'Huot', 'Doctor', 'Cardiologist', '089228246', 4, 1);
